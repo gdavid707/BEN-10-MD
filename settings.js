6a424c645788e44779c48b8e55a198a2e9436fb6
gdavid707 <galaxydavid877@gmail.com>
2025-07-24 01:19:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: '',
   author: '‎',
   botName: "𝗕𝗘𝗡 10 𝗠𝗗",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Davi', // Your name
+  ownerNumber: '2560701864734', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
